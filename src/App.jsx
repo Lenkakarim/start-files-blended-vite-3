0dd19d74ae18001cc3489a47366cbfe2a47a1f7f
Len4ik <karimovaolena@yahoo.com>
2025-04-27 10:18:26 -0400

    add task

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
-import Heading from './components/Heading/Heading';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header/Header';
+import { lazy, Suspense } from 'react';
+import Loader from './components/Loader/Loader';
+
+const Home = lazy(() => import('./pages/Home'));
+const Country = lazy(() => import('./pages/Country'));
+const SearchCountry = lazy(() => import('./pages/SearchCountry'));
 
 export const App = () => {
-  return <Heading title="App" bottom />;
+  return (
+    <div>
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country" element={<SearchCountry />} />
+          <Route path="/country/:countryId" element={<Country />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
